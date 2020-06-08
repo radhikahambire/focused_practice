d3a65e37cb5a1086870e5ac74103c002fce0ea5c
radhikahambire <65546265+radhikahambire@users.noreply.github.com>
2020-06-08 20:04:49 +0530

    Update reporter.js

--- Support/reporter.js
+++ Support/reporter.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const mkdirp = require("mkdirp");
 const reporter = require("cucumber-html-reporter");
 const moment = require("moment");
-const html= "reports_" + moment().format("DD_MM_YYYY_HH_mm");
-const htmlReports =  process.cwd() + "/reports/" + html + "/";
-const targetJson = process.cwd() + "/reports/" + html + "/json/focused_practice_report.json";
+// const html= "reports_" + moment().format("DD_MM_YYYY_HH_mm");
+const htmlReports =  process.cwd() + "/reports"  + "/";
+const targetJson = process.cwd() + "/reports" + "/json/focused_practice_report.json";
 
 const cucumberReporterOptions = {
     theme : "bootstrap",
